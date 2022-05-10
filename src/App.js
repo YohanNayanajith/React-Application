4e03f3f901439636c6088a9a1a2b353d45617759
YohanNayanajith <yohannayanajith40@gmail.com>
2022-05-10 23:34:31 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
+import TestExpenseItem from "./components/TestExpenseItem/TestExpenseItem";
+import Card from "./components/UI/Card";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
+  const expense = [
+    {id:'e1',title:'New Desk',amount:450,date: new Date(2022,4,4)},
+    {id:'e2',title:'New Chair',amount:500,date: new Date(2022,4,10)},
+    {id:'e3',title:'New Document Stuff',amount:600,date: new Date(2022,4,22)},
+    {id:'e4',title:'New Window',amount:200,date: new Date(2022,4,30)},
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Card className="expenses">
+      <NewExpense />
+      <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date} />
+      <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date} />
+      <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date} />
+      <ExpenseItem title={expense[3].title} amount={expense[3].amount} date={expense[3].date} />
+
+      {/* <TestExpenseItem items={expense} /> */}
+    </Card>
   );
 }
 
---
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate'
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+    // const expenseDate = new Date(2021, 2, 28);
+    // const expenseTitle = 'Car Insuarance';
+    // const expenseAmount = 294.67;
+    const [title,setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle("Updated!");
+    };
+
+    return (
+        <Card className="expense-item">
+            {/* <div>{props.date.toISOString()}</div> */}
+            <ExpenseDate date = {props.date} />
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+            {/* <button onClick={()=>{console.log("Clicked")}}>Change Title</button> */}
+            <button onClick={clickHandler}>Change Title</button>
+        </Card>
+    )
+}
+
+export default ExpenseItem;
